a12a8926afdf9a168418149335eed24544eeab9c
priyank21116 <priyank21116@gmail.com>
2022-08-31 21:11:09 +0530

    'vedio_enabled

--- src/Components/VideoLive.js
+++ src/Components/VideoLive.js
@@ -6,30 +6,30 @@ import Webcam from "react-webcam";
 
 const VideoLive = () => {
 
-      //create model and action state
-      // const [model, setModel] = useState(null)
-      // const [action, setAction] = useState(null)
-      // const [labels, setLabels] = useState(null)
+      // create model and action state
+      const [model, setModel] = useState(null)
+      const [action, setAction] = useState(null)
+      const [labels, setLabels] = useState(null)
 
-      // const loadModel = async () => {
-      //       const recognizer = await speech.create("BROWSER_FFT")
-      //       console.log("Model Loaded")
-      //       // console.log("RECO",recognizer)
+      const loadModel = async () => {
+            const recognizer = await speech.create("BROWSER_FFT")
+            console.log("Model Loaded")
+            // console.log("RECO",recognizer)
 
-      //       // await recognizer.ensureModelLoaded()
-      //       console.log("RECO2", recognizer)
-      //       console.log(recognizer.wordLabels())
-      //       setModel(recognizer)
-      //       setLabels(recognizer.words);
-      //       console.log("LABELS", labels)
-      // }
-      // console.log("LABELS", labels)
+            // await recognizer.ensureModelLoaded()
+            console.log("RECO2", recognizer)
+            console.log(recognizer.wordLabels())
+            setModel(recognizer)
+            setLabels(recognizer.words);
+            console.log("LABELS", labels)
+      }
+      console.log("LABELS", labels)
 
-      // useEffect(() => { loadModel() }, [])
+      useEffect(() => { loadModel() }, [])
 
       return (
             <Paper elevation={12} style={{ backgroundColor: "#DAF76E", borderRadius: 15, height: 250, justifyContent: "center", textAlign: "center", overflow: "hidden" }} >
-                  {/* <Webcam height={250} /> */}
+                  <Webcam height={250} />
             </Paper>
       )
 }
